1da9b7e80f49a047e44c48486151a4961f9d6a76
GitHub-0a <148424822+GitHub-0a@users.noreply.github.com>
2023-11-04 11:07:45 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,8 @@
+const taiJyuu = 65;   /*体重*/
+const shinCyou = 1.7; /*身長*/
+let bMi;              /*BMI*/
+bMi = taiJyuu / (shinCyou * shinCyou);
+
+console.log('体重:',taiJyuu,'kg'); /*体出力*/
+console.log('身長:',shinCyou,'m'); /*身長出力*/
+console.log('BMI :',bMi);          /*BMI出力*/
